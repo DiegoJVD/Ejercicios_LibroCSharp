e491d1570c3a95e5bd5e9f4bb5663531a89ed4c1
Ne0h0 <67572551+Ne0h0@users.noreply.github.com>
2020-09-07 21:47:36 -0400

    commit inicial

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Ejercicios_LibroCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Enel!");
         }
     }
 }
